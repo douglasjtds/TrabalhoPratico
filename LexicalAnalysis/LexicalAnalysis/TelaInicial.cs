932e96b6a376c77f434195ca9272cbbb2756d576
Matheus Pires <matheuswith51@hotmail.com>
2018-03-15 01:20:41 -0300

    trying read .txt
    not working yet

--- LexicalAnalysis/LexicalAnalysis/TelaInicial.cs
+++ LexicalAnalysis/LexicalAnalysis/TelaInicial.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,32 +30,47 @@ namespace LexicalAnalysis
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string codePath = Path.Combine(Environment.CurrentDirectory, "ErrorCase1.txt");
+            MessageBox.Show(codePath);
+
+            if (File.Exists(codePath))
+            {
+                Stream entrada = File.Open(codePath, FileMode.Open);
+                StreamReader readText = new StreamReader(entrada);
+                int numRow = Convert.ToInt32(readText.ReadLine());
+
+                for (int i = 0; i < numRow; i++)
+                {
+                    MessageBox.Show(readText.ReadLine());
+                }
+
+            }
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            string codePath = Path.Combine(Environment.CurrentDirectory, "ErrorCase2.txt");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            string codePath = Path.Combine(Environment.CurrentDirectory, "ErrorCase3.txt"); 
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            e.GetType();
+            string codePath = Path.Combine(Environment.CurrentDirectory, "SuccessCase1.txt");
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-
+            string codePath = Path.Combine(Environment.CurrentDirectory, "SuccessCase2.txt");
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            string codePath = Path.Combine(Environment.CurrentDirectory, "SuccessCase3.txt");
         }
     }
 }
